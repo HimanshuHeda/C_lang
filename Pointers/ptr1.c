c00e83a35bbea2f866dd85bf78fec9ae00018d43
HimanshuHeda <harsh967896@gmail.com>
2024-12-20 00:09:07 +0530

    pointer output and format

--- Pointers/ptr1.c
+++ Pointers/ptr1.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-void modifyArray(int *arr, int size) {
-    for (int i = 0; i < size; i++) {
+void modifyArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         // Modify each element by adding 5
         *(arr + i) += 5; // or you can use arr[i] += 5;
     }
 }
 
-int main() {
-    int numbers[] = {1, 2, 3, 4, 5}; // Declare and initialize an array
+int main()
+{
+    int numbers[] = {1, 2, 3, 4, 5};                 // Declare and initialize an array
     int size = sizeof(numbers) / sizeof(numbers[0]); // Calculate the size of the array
 
     printf("Before modification:\n");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", numbers[i]);
     }
     printf("\n");
@@ -21,7 +25,8 @@ int main() {
     modifyArray(numbers, size);
 
     printf("After modification:\n");
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         printf("%d ", numbers[i]);
     }
     printf("\n");
